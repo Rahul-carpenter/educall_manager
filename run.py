071ded725e79457a0e51a02618a95a05c0c0fcec
Rj33 <rj33@Linux.myguest.virtualbox.org>
2025-12-13 19:22:26 +0530

    upgradedb

--- run.py
+++ run.py
@@ -1,19 +1,7 @@
 from app import app, db
-from flask_migrate import Migrate, upgrade
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
+from flask_migrate import Migrate
 
-# 🔧 Set timezone for each DB connection
-@event.listens_for(Engine, "connect")
-def set_timezone(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("SET TIME ZONE 'Asia/Kolkata';")
-    cursor.close()
-
-# Initialize Flask-Migrate
-migrate_obj = Migrate(app, db)
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
-    with app.app_context():
-        upgrade()  # run migrations
     app.run(host="0.0.0.0", port=5000)
